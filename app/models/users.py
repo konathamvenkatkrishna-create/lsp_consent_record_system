76c101cad85d23a99a1d2e9b90cb43c288a61348
konathamvenkatkrishna-create <konathamvenkatkrishna@gmail.com>
2026-02-26 11:43:07 +0530

    consent capture

--- app/models/users.py
+++ app/models/users.py
@@ -1,17 +1,3 @@
-# from sqlalchemy import  Column, BigInteger,String, Text
-# from app.core.db import Base
-
-# class User(Base):
-#     __tablename__ = "users"
- 
-#     id = Column(BigInteger, primary_key=True)
-#     # username = Column(String(50),unique=True)
-#     # mobile_number = Column(String(25), unique=True)
-#     # password_hash = Column(String)
-#     # device_id = Column(Text,nullable=True)
-#     # role = Column(String,default="USER")
-
-
 from sqlalchemy import Column, BigInteger, String, Text
 from app.core.db import Base
 
